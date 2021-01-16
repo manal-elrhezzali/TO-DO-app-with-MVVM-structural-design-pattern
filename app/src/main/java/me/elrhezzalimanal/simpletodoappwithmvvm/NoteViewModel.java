72dc8d8e47c2a47563b835d6eef87a0c108e173c
Manal El-rhezzali <manal.gzl99@gmail.com>
2021-01-16 16:40:21 +0100

    added NoteViewModel

---
+++ app/src/main/java/me/elrhezzalimanal/simpletodoappwithmvvm/NoteViewModel.java
@@ -0,0 +1,47 @@
+package me.elrhezzalimanal.simpletodoappwithmvvm;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class NoteViewModel extends AndroidViewModel {
+
+    private NoteRepository repository;
+    private LiveData<List<Note>> allNotes;
+
+    public NoteViewModel(@NonNull Application application) {
+        super(application);
+        repository = new NoteRepository((application));
+        allNotes = repository.getAllNotes();
+    }
+
+    // our Activities and fragments "Views" will only have reference to the ViewModel not to the NoteRepository
+    // that is why we create wrapper methods for our database operations methods of NoteRepository
+    public void insert(Note note){
+        repository.insert(note);
+    }
+
+    public void update(Note note){
+        repository.update(note);
+    }
+
+    public void delete(Note note){
+        repository.delete(note);
+    }
+
+    public void deleteAllNotes(){
+        repository.deleteAllNotes();
+    }
+
+    public LiveData<List<Note>> getAllNotes(){
+        return repository.getAllNotes();
+
+    }
+
+    
+
+}
