bbb49ccb247aeec1505ebe874ce596e25181c0b1
Manal El-rhezzali <manal.gzl99@gmail.com>
2021-01-16 18:19:55 +0100

    implemented the onChanged method of the Observer interface

--- app/src/main/java/me/elrhezzalimanal/simpletodoappwithmvvm/MainActivity.java
+++ app/src/main/java/me/elrhezzalimanal/simpletodoappwithmvvm/MainActivity.java
@@ -3,6 +3,8 @@ package me.elrhezzalimanal.simpletodoappwithmvvm;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
@@ -17,6 +19,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);//set it to true if you know that the recyclerView's size won't change ,it will make it more efficient
+
+        // final NoteAdapter adapter = new NoteAdapter();
+        NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
 
         noteViewModel = new ViewModelProvider(this,
                 ViewModelProvider.AndroidViewModelFactory.getInstance(this.getApplication())
@@ -34,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
             public void onChanged(List<Note> notes) {
                 //update RecyclerView
                 Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+                adapter.setNotes(notes);
             }
         });
     }
